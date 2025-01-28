293719d1c77325a561bbd3341f1b459b67c23bcc
Pratik <pratikkawade914@gmail.com>
2025-01-28 23:45:06 +0530

    Initial commit for AcciAlert Spring Boot project

---
+++ AcciAlert_server/src/test/java/com/TroubleShooters/AcciAlert_server/AcciAlertServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TroubleShooters.AcciAlert_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcciAlertServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/com/TroubleShooters/AcciAlert_server/AcciAlertServerApplication.java
@@ -0,0 +1,13 @@
+package com.TroubleShooters.AcciAlert_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcciAlertServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcciAlertServerApplication.class, args);
+	}
+
+}
