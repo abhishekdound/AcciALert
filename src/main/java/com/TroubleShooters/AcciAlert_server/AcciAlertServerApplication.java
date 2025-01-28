95427174e4eb7062beac6ab984351b84e65a1397
Pratik <pratikkawade914@gmail.com>
2025-01-29 03:07:14 +0530

    second commit, hospital and user model added

--- src/main/java/com/TroubleShooters/AcciAlert_server/AcciAlertServerApplication.java
+++ src/main/java/com/TroubleShooters/AcciAlert_server/AcciAlertServerApplication.java
@@ -2,8 +2,10 @@ package com.TroubleShooters.AcciAlert_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.TroubleShooters.AcciAlert_server.model")
 public class AcciAlertServerApplication {
 
 	public static void main(String[] args) {
